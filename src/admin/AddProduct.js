bb25f2a82e4cf630c775ac387fa7bafc204302fd
sadakchap <aliceprerna@gmail.com>
2020-11-07 16:06:45 +0530

    add Product implemented

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -1,32 +1,104 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Link, Redirect } from 'react-router-dom';
+import { isAuthenticated } from '../auth/helper';
 import Base from '../core/Base'
+import { createProduct, getCategories } from './helper/adminapicall';
 
 const AddProduct = () => {
 
-    const [formData, setFormData] = useState({
-        file: null,
+    const { user, token } = isAuthenticated();
+
+    const [values, setValues] = useState({
         name: '',
         desc: '',
         price: '',
         stock: '',
+        photo: '',
+        categories: '',
+        category: '',
+        loading: false,
+        error: '',
+        createdProduct: '',
+        getRedirect: false,
+        formData: ''
     });
 
-    const handleChange = name => e => setFormData({...FormData, [name]: e.target.value});
+    const { name, desc, price, stock, categories, loading, error, createdProduct, getRedirect, formData } = values;
+
+    const handleChange = name => e => {
+        const value = name === 'photo' ? e.target.files[0] : e.target.value;
+        formData.set(name, value);
+        setValues({ ...values, [name]: value });
+    };
+    
+    
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log('submitting form!');
+        setValues({...values, error: "", loading: true });
+        createProduct(user._id, token, formData).then(data => {
+            if(data.error){
+                setValues({ ...values, error: data.error });
+            }else{
+                setValues({
+                    ...values,
+                    name: '',
+                    desc: '',
+                    price: '',
+                    stock: '',
+                    file: '',
+                    category: '',
+                    createdProduct: data.name,
+                    loading: true
+                });
+                setTimeout(() => {
+                    setValues({ ...values, getRedirect: true})
+                }, 2000);
+            }
+        })
+    }
+    
+    const preload = () => {
+        getCategories().then(data => {
+            console.log(data);
+            if(data.error){
+                setValues({ ...values, error: data.error })
+            }else{
+                setValues({ ...values, categories: data, formData: new FormData() });
+                console.log("Categories -> ", categories);
+            }
+        }).catch(err => {
+            console.log(err);
+            setValues({ ...values, error: err.response.data.error})
+        })
     }
 
-    const { name, desc, price, stock } = formData;
+    useEffect(() => {
+        preload();
+        // eslint-disable-next-line
+    }, []);
+
+    const successMessage = () => (
+        <div className="alert alert-success mt-3" style={{ display: createdProduct ? '' : 'none'}}>
+            Product "{createdProduct}" created Successfully!
+        </div>
+    )
+
+    const errorMessage = () => (
+        <div className="alert alert-danger mt-3" style={{ display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    )
 
     const productForm = () => (
         <form>
+            {successMessage()}
+            {errorMessage()}
+            {getRedirect && <Redirect to="/admin/dashboard" />}
             <span>Post Photo</span>
             <div className="form-group">
                 <label className="btn btn-success btn-block">
-                    <input type="file" onChange={handleChange("file")} name="photo" accept="image" placeholder="choose a file" />
+                    <input type="file" onChange={handleChange("photo")} name="photo" accept="image" placeholder="choose a file" />
                 </label>
             </div>
             <div className="form-group">
@@ -41,21 +113,23 @@ const AddProduct = () => {
                 </div>
                 <div className="form-group col-lg-4">
                     <select onChange={handleChange("category")} className="form-control" placeholder="Category">
-                        <option value="a">A</option>
-                        <option value="b">B</option>
+                        <option>Select</option>
+                        { categories && categories.map((cate, idx) => (
+                            <option key={idx} value={cate._id}>{cate.name}</option>
+                        ))}
                     </select>
                 </div>
                 <div className="form-group col-lg-4">
                     <input type="number" onChange={handleChange("stock")} placeholder="Quantity" className="form-control" value={stock} placeholder="Quantity" />
                 </div>
             </div>
-            <button type="submit" onClick={handleSubmit} className="btn btn-outline-success">Create Product</button>
+            <button type="submit" disabled={loading} onClick={handleSubmit} className="btn btn-outline-success">Create Product</button>
         </form>
     )
 
     return (
         <Base title="Add Products" description="Here, you can add new products" className="bg-info p-4 text-light">
-            <Link to="/admn/dashboard" className="btn btn-md btn-dark mb-3">Admin Home</Link>
+            <Link to="/admin/dashboard" className="btn btn-md btn-dark mb-3">Admin Home</Link>
             <div className="row bg-dark text-light rounded py-3">
                 <div className="col-md-8 offset-md-2">
                     <h2>Create Products</h2>
